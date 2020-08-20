2479b6a24c562f3499b8b70a646e7d4a94849684
hkovacs <hk@hkovacs.com>
2020-08-20 12:16:52 +0300

    wrapped error reporting in host check

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+if ($_SERVER["HTTP_HOST"] == 'pct.test') {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
 
 require_once('../app/bootstrap.php');
 
